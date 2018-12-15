0cd9631891f16f6b9660eecc63eca24086fc2fd8
諒安藤 <andrew.charcopin@gmail.com>
2018-12-16 01:51:37 +0900

    change login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,27 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def sign_in
+    @user = User.new
+  end
+
+  def sign_up
+    @user = User.new
+  end
+
+  def registration
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to nayame_index_path
+    else
+      redirect_to sign_up_path
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
 
   resources :lp, only: [:index]
   resources :nayame, only: [:index, :show, :edit]
+
+
+  post 'session', to: 'users#session'
+  post 'registration', to: 'users#registration'
+  get 'sign_in', to: 'users#sign_in'
+  get 'sign_up', to: 'users#sign_up'
 
 end
