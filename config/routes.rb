bbdeb5c2c9f6c1e161ba489d30c78f2b079989ea
諒安藤 <andrew.charcopin@gmail.com>
2018-12-27 15:34:59 +0900

    update devise toastr

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root :to => "lp#index"
+  devise_for :users, controllers: {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
 
   resources :lp, only: [:index]
   resources :nayame, only: [:index, :show, :edit]
-
-
-  post 'auth', to: 'users#auth'
-  post 'registration', to: 'users#registration'
-  get 'sign_in', to: 'users#sign_in'
-  get 'sign_up', to: 'users#sign_up'
 
 end
